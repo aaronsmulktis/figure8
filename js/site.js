74821ad7fddb4618366f9dd8fdc9ec1dd1e300c5
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-09-14 13:14:45 -0400

    .

--- js/site.js
+++ js/site.js
@@ -136,11 +136,11 @@ jQuery(document).ready(function($) {
 
     function onScroll(event){
 	    var scrollPos = $(document).scrollTop();
-	    $('#main-nav a').each(function () {
+	    $('.navbar-default .navbar-nav>li>a').each(function () {
 	        var currLink = $(this);
 	        var refElement = $(currLink.attr("href"));
 	        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-	            $('#main-nav ul li a').removeClass("active"); //added to remove active class from all a elements
+	            $('.navbar-default .navbar-nav>li>a').removeClass("active"); //added to remove active class from all a elements
 	            currLink.addClass("active");
 	        }
 	        else{
