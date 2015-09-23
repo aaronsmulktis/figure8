747a920482dede8a7fad6ab5b3a450ef9fea41ea
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-09-23 16:40:32 -0400

    .

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -3,6 +3,9 @@
 		<nav id="main-nav" class="navbar navbar-default navbar-fixed-top shift" role="navigation">
 			<div class="container-fluid">
 				<div class="navbar-header">
+					<div id="mob-brand" class="navbar-brand pull-left visible-xs">
+						<a id="logo" href="#content"><span>((8))</span></a>
+					</div>
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse" aria-expanded="false" aria-controls="navbar-collapse">
 					<i class="fa fa-arrow-down"></i>
 					</button>
